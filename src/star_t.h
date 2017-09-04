d6c2f2fd957259474dd846ac593d27561c3f2958
Anderson Antunes <anderson.utf@gmail.com>
2017-09-04 19:07:56 -0300

    Use C linkage in C++ compiler (arduino-mk)

--- src/star_t.h
+++ src/star_t.h
@@ -1,6 +1,10 @@
 #include <stdint.h>
 #include <tokens.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifndef STAR_T_H
 #define STAR_T_H
 
@@ -49,5 +53,9 @@ typedef struct {
 
 void begin();
 int8_t run(uint8_t* code);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
